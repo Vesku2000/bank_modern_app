9893dfbafb3d84d40e1e67d9d570b53edf6ea189
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2022-08-13 14:48:30 +0300

    components added

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -3,8 +3,29 @@ import styles from "./style"
 const App = () => (
     
       <div className='bg-primary w-full overflow-hidden'>
-        <div className={`${styles.padding}`}>
+        <div className={`${styles.padding} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            NAVBAR
+          </div>
+        </div>
 
+        <div className={` bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            HERO
+          </div>
+        </div>
+
+        <div className={` bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            Stats
+            Busines
+            Billing
+            CardDeal
+            Testimonial
+            Clients
+            CTA
+            Footer
+          </div>
         </div>
       </div>
     
