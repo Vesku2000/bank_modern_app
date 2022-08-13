df43fe22cb95697dcb1c252a256c02c1ec3f6cad
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2022-08-13 18:09:32 +0300

    Navbar changes

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,30 +1,33 @@
-import styles from "./style"
+import styles from "./style";
+
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components";
 
 const App = () => (
     
       <div className='bg-primary w-full overflow-hidden'>
         <div className={`${styles.padding} ${styles.flexCenter}`}>
           <div className={`${styles.boxWidth}`}>
-            NAVBAR
+            <Navbar />
+            
           </div>
         </div>
 
         <div className={` bg-primary ${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`}>
-            HERO
+            <Hero />
           </div>
         </div>
 
         <div className={` bg-primary ${styles.paddingX} ${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`}>
-            Stats
-            Busines
-            Billing
-            CardDeal
-            Testimonial
-            Clients
-            CTA
-            Footer
+           <Stats />
+           <Business />
+           <Billing />
+           <CardDeal />
+           <Testimonials />
+           <Clients />
+           <CTA />
+           <Footer />
           </div>
         </div>
       </div>
