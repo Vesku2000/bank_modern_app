fef5abc5ab1e1cbf415b73535b645d62a6674119
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2022-08-13 14:25:56 +0300

    name changes

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import styles from "./style"
 
 const App = () => (
     
       <div className='bg-primary w-full overflow-hidden'>
-        <div>
-          
+        <div className={`${styles.padding}`}>
+
         </div>
       </div>
     
